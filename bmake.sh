05cdde30957471869a2daecda7afb2d74ce3ac96
holishing <holishingtest@gmail.com>
2018-05-25 01:12:48 +0800

    bmake.sh: change build patch in /tmp

--- bmake.sh
+++ bmake.sh
@@ -1,3 +1,3 @@
-cd $HOME && \
+cd /tmp && \
 curl -L http://crufty.net/ftp/pub/sjg/bmake-20171207.tar.gz|tar -zxv && \
 cd bmake && ./boot-strap prefix=/usr/local && ./boot-strap prefix=/usr/local op=install
