0c50b5522d7a8187143955653c5d72bcd7dbe555
holishing <holishingtest@gmail.com>
2018-11-21 01:42:27 +0800

    Update BBS release version to v0.96.1

--- buildbbs.sh
+++ buildbbs.sh
@@ -1,13 +1,13 @@
-sh -c "curl -L https://github.com/ccns/dreambbs_snap/archive/v0.96.tar.gz|tar -zxv" \
-        && mv dreambbs_snap-0.96 ./$USER \
+sh -c "curl -L https://github.com/ccns/dreambbs_snap/archive/v0.96.1.tar.gz|tar -zxv" \
+        && mv dreambbs_snap-0.96.1 ./$USER \
         && cp -r ./$USER /home/ \
         && rm -rf ./$USER
-curl -L https://github.com/ccns/dreambbs/archive/v0.96.tar.gz | tar -zxv -C $HOME
-cd $HOME/dreambbs-0.96 \
+curl -L https://github.com/ccns/dreambbs/archive/v0.96.1.tar.gz | tar -zxv -C $HOME
+cd $HOME/dreambbs-0.96.1 \
         && cp sample/dreambbs.conf ./dreambbs.conf \
         && sed -i 's|\/home\/bbs|'$HOME'|g' dreambbs.conf 
 #vim -c 'set fenc=big5 enc=big5 tenc=utf8' -c 'e!' dreambbs.conf
-cd $HOME/dreambbs-0.96 \
+cd $HOME/dreambbs-0.96.1 \
         && bmake all install clean \
         && sh $HOME/sh/start.sh \
         && $HOME/bin/bbsd 23
